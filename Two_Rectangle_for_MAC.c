8356e052d99f57b1a2713807bf33bc6f4965e81d
Dimitrool <92887696+Dimitrool@users.noreply.github.com>
2022-06-20 14:43:22 +0200

    commit 1

--- Two_Rectangle_for_MAC.c
+++ Two_Rectangle_for_MAC.c
@@ -11,15 +11,15 @@ struct coordinates{
 };
 
 struct directions{
-    int down;
     int up; 
+    int down;
     int left;
     int right;
 };
 
 int Reading(int *x1, int *y1, int *x2, int *y2, int n){
     printf("Enter the coordinates of the rectangle number %d (upper left corner, lower right corner): ", n);
-    if(scanf("%d,%d,%d,%d", x1, y1, x2, y2) != 4 || getchar()!='\n' || !(*x1>=0 && *x1<=79) || !(*y1>=0 && *y1<=79) || !(*x2>=0 && *x2<=79) || !(*y2>=0 && *y2<=79) || *x1 >= *x2 || *y1 >= *y2){
+    if(scanf("%d,%d,%d,%d", x1, y1, x2, y2) != 4 || getchar()!='\n' || !(*x1>=0 && *x1<=79) || !(*y1>=0 && *y1<=79) || !(*x2>=0 && *x2<=79) || !(*y2>=0 && *y2<=79) || (*x1 >= *x2) || (*y1 <= *y2)){
         return -1;
     }
     return 0;
@@ -27,18 +27,18 @@ int Reading(int *x1, int *y1, int *x2, int *y2, int n){
 
 void Create_symbol(struct directions *symbol, struct coordinates *rectangles, int t, int x, int y){
     for(int i = 0;i < t;i++){
-           if(rectangles[i].x1 <= x && rectangles[i].x2 > x && (y == rectangles[i].y1 || y == rectangles[i].y2)){
-                symbol->right = 1;
-           }
-           if(rectangles[i].x1 < x && rectangles[i].x2 >= x && (y == rectangles[i].y1 || y == rectangles[i].y2)){
-                symbol->left = 1;
-           }
-           if(rectangles[i].y1 >= y && rectangles[i].y2 < y && (x == rectangles[i].x1 || x == rectangles[i].x2)){
-                symbol->down = 1;
-           }
-           if(rectangles[i].y1 > y && rectangles[i].y2 <= y && (x == rectangles[i].x1 || x == rectangles[i].x2)){
-                symbol->up = 1;
-           }
+        if(rectangles[i].x1 <= x && rectangles[i].x2 > x && (y == rectangles[i].y1 || y == rectangles[i].y2)){
+            symbol->right = 1;
+        }
+        if(rectangles[i].x1 < x && rectangles[i].x2 >= x && (y == rectangles[i].y1 || y == rectangles[i].y2)){
+            symbol->left = 1;
+        }
+        if(rectangles[i].y1 >= y && rectangles[i].y2 < y && (x == rectangles[i].x1 || x == rectangles[i].x2)){
+            symbol->down = 1;
+        }
+        if(rectangles[i].y1 > y && rectangles[i].y2 <= y && (x == rectangles[i].x1 || x == rectangles[i].x2)){
+            symbol->up = 1;
+        }
     }
 }
 
@@ -94,7 +94,13 @@ void Print_symbol(struct directions symbol){
 int main(){
     setlocale(LC_ALL, "ru_RU.utf-8");
 
-    int t = 2;
+    int t;
+    printf("Enter the number of rectangles: ");
+    if(scanf("%d", &t) != 1 || getchar() != '\n' || t<2){
+        printf("Bad input!\n");
+        return -1;
+    }
+
     int max_x = 0, max_y = 0;
     struct coordinates rectangles[t];
 
